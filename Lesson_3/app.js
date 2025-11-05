7414a45d76e0903dc93f72a50db642ead9580cf4
i3oi3ka <i3oi3ka@ukr.net>
2025-11-05 22:19:14 +0200

    Add middleware for logging requests

--- Lesson_3/app.js
+++ Lesson_3/app.js
@@ -1,6 +1,11 @@
 import express from "express";
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("Наше проміжне ПЗ");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
