ec4e33602d71c0fb83568654a3d7cdec63c069cd
i3oi3ka <i3oi3ka@ukr.net>
2025-11-09 18:01:31 +0200

    Refactor code structure for improved readability and maintainability

---
+++ Lesson_4/todo/app.js
@@ -0,0 +1,30 @@
+import express, { json } from "express";
+import cors from "cors";
+import routerApi from "./api";
+const app = express();
+// parse application/json
+app.use(json());
+// cors
+app.use(cors());
+app.use("/api", routerApi);
+app.use((_, res, __) => {
+  res.status(404).json({
+    status: "error",
+    code: 404,
+    message: "Use api on routes: /api/tasks",
+    data: "Not found",
+  });
+});
+app.use((err, _, res, __) => {
+  console.log(err.stack);
+  res.status(500).json({
+    status: "fail",
+    code: 500,
+    message: err.message,
+    data: "Internal Server Error",
+  });
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
