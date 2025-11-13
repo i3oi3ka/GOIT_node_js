c2f6f8cbaa836074c72abf095ac325ed7ce67c1a
i3oi3ka <i3oi3ka@ukr.net>
2025-11-13 22:23:57 +0200

    Add database connection setup using Sequelize

---
+++ Lesson_5/con_db.js
@@ -0,0 +1,18 @@
+import { Sequelize } from 'sequelize';
+
+// Варіант 1: передача `URI` для підключення
+const sequelize = new Sequelize('sqlite::memory:') // для `sqlite`
+const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // для `postgres`
+
+// Варіант 2: передача параметрів окремо
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'path/to/database.sqlite'
+})
+
+// Варіант 2: передача параметрів окремо (для інших діалектів)
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: /* 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+})
+
