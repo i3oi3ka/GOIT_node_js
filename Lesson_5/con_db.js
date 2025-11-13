86a7c4a158d68a558e380483481ae42df7355274
i3oi3ka <i3oi3ka@ukr.net>
2025-11-13 22:25:44 +0200

    Refactor database connection code to comment out example configurations

--- Lesson_5/con_db.js
+++ Lesson_5/con_db.js
@@ -1,14 +1,14 @@
 import { Sequelize } from 'sequelize';
 
-// Варіант 1: передача `URI` для підключення
-const sequelize = new Sequelize('sqlite::memory:') // для `sqlite`
-const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // для `postgres`
+// // Варіант 1: передача `URI` для підключення
+// const sequelize = new Sequelize('sqlite::memory:') // для `sqlite`
+// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // для `postgres`
 
-// Варіант 2: передача параметрів окремо
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'path/to/database.sqlite'
-})
+// // Варіант 2: передача параметрів окремо
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: 'path/to/database.sqlite'
+// })
 
 // Варіант 2: передача параметрів окремо (для інших діалектів)
 const sequelize = new Sequelize('database', 'username', 'password', {
@@ -16,3 +16,9 @@ const sequelize = new Sequelize('database', 'username', 'password', {
   dialect: /* 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
 })
 
+try {
+await sequelize.authenticate()
+  console.log('З'єднання з БД було успішно встановлено')
+}catch (e) {
+  console.log('Неможливо виконати підключення до БД: ', e)
+}
