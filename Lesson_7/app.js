ecd230a0e49b38d100944cac26cbe67c7ada5b8d
i3oi3ka <i3oi3ka@ukr.net>
2025-11-19 23:28:36 +0200

    Add user registration and login routes with JWT token generation

--- Lesson_7/app.js
+++ Lesson_7/app.js
@@ -24,3 +24,58 @@ passport.use(
       .catch((err) => done(err));
   })
 );
+
+router.post("/registration", async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    return res.status(409).json({
+      status: "error",
+      code: 409,
+      message: "Email is already in use",
+      data: "Conflict",
+    });
+  }
+  try {
+    const newUser = new User({ username, email });
+    newUser.setPassword(password);
+    await newUser.save();
+    res.status(201).json({
+      status: "success",
+      code: 201,
+      data: {
+        message: "Registration successful",
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user || !user.validPassword(password)) {
+    return res.status(400).json({
+      status: "error",
+      code: 400,
+      message: "Incorrect login or password",
+      data: "Bad request",
+    });
+  }
+
+  const payload = {
+    id: user.id,
+    username: user.username,
+  };
+
+  const token = jwt.sign(payload, secret, { expiresIn: "1h" });
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      token,
+    },
+  });
+});
